22c9f804f647e48e75b4c93209d75d4935f25dc3
tomomimiya <tomomimiya128@gmail.com>
2022-11-17 15:41:55 -0700

    added writefile function

--- index.js
+++ index.js
@@ -1,60 +1,74 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 //questions
-inquirer
-  .prompt([
-    {
-      type: "input",
-      name: "fileTitle",
-      message: "Title of this new file we are creating? (required)",
-    },
-    {
-      type: "input",
-      name: "projectTitle",
-      message: "Title of your project?",
-    },
-    {
-      type: "input",
-      name: "description",
-      message: "Description of it? (required)",
-    },
-    {
-      type: "input",
-      name: "installation",
-      message:
-        "List any required packages for installation of this application",
-    },
-    {
-      type: "input",
-      name: "usage",
-      message: "Usage, or example of this project",
-    },
-    {
-      type: "checkbox",
-      name: "license",
-      message: "Choose the license",
-      choices: ["Apache", "MIT", "GPL", "none"],
-    },
-    {
-      type: "input",
-      name: "contributing",
-      message: "Enter your guideline for contributing",
-    },
-    {
-      type: "input",
-      name: "tests",
-      message: "how do you test this application?",
-    },
-    {
-      type: "input",
-      name: "question",
-      message: "Your contact info",
-    },
-  ])
-  .then((responses) => {
-    const filename = `${responses.name.toLowercase()}.md`;
-    fs.writeFile(filename, JSON.stringify(responses, null, 2), (err) =>
-      err ? console.log(err) : console.log("success!")
-    );
-  });
+inquirer.prompt([
+  {
+    type: "input",
+    name: "fileTitle",
+    message: "Title of this new file we are creating? (required)",
+  },
+  {
+    type: "input",
+    name: "projectTitle",
+    message: "Title of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Description of it? (required)",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "List any required packages for installation of this application",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Usage, or example of this project",
+  },
+  {
+    type: "checkbox",
+    name: "license",
+    message: "Choose the license",
+    choices: ["Apache", "MIT", "GPL", "none"],
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Enter your guideline for contributing",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "how do you test this application?",
+  },
+  {
+    type: "input",
+    name: "question",
+    message: "Your contact info",
+  },
+]);
+
+function writeToFile(fileName, data) {
+  fs.appendFile(`${fileName}.md`, data, (err) =>
+    err ? console.error(err) : console.log(`${fileName}.md has been generated.`)
+  );
+}
+
+//function to initialize
+async function init() {
+  let answers = await userInput();
+  writeToFile(answers.fileName, generateMarkdown(answers));
+}
+
+init();
+
+//   .then((responses) => {
+//     const filename = `${responses.name.toLowercase()}.md`;
+//     fs.writeFile(filename, JSON.stringify(responses, null, 2), (err) =>
+//       err ? console.log(err) : console.log("success!")
+//     );
+//   });
